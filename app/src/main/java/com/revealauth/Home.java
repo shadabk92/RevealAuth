410c33dac734192acbc48109c3784ff6da4d4b44
shadabk <shadab.khatib@gmail.com>
2017-12-21 01:18:35 +0530

    awesome login screen

--- app/src/main/java/com/revealauth/Home.java
+++ app/src/main/java/com/revealauth/Home.java
@@ -5,17 +5,16 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
 
-import butterknife.BindView;
-
 public class Home extends AppCompatActivity {
 
-    @BindView(R.id.btn_start)
     Button btn_start;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        btn_start = findViewById(R.id.btn_start);
 
         btn_start.setOnClickListener(view -> {
             Intent intent = new Intent(getApplicationContext(),FillParagraphActivity.class);
